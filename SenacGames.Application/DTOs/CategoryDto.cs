e27b8e903e82d4b93fccf59a0eed1503ae229c9e
ANNA JULIA MARCELINO OLIVEIRA <anna.jmoliveira3@senacsp.edu.br>
2026-05-29 14:43:19 -0300

    SenacGames

---
+++ SenacGames.Application/DTOs/CategoryDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+ // dito de categoria ,para ser usado na hora de cadastrar um jogo,para o usuario selecionar a categoria do jogo 
+namespace SenacGames.Application.DTOs
+{
+ public class CategoryDto
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// quantidade de games nesta categoria.
+        /// util para mostrar no dashboard e na listagem.
+        /// </summary>
+        public int GamesCount { get; set; }
+    }
+    /// <summary>
+    ///     DTO para criaçao de uma nova categoria.
+    /// </summary>
+    public class CreateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+ 
+   }
+    /// <summary>
+    /// DTO para atualização de uma categoria existente.
+    /// </summary>
+    public class UpdateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
+
---
+++ SenacGames.Application/Interfaces/IGameService.cs
@@ -0,0 +1,23 @@
+﻿using SenacGames.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenacGames.Application.Interfaces
+{
+    /// <summary>
+    ///     contrato de serviço de games.
+    ///     define as operaçoes de negocio disponiveis para os games.
+    /// </summary>
+     public interface IGameService
+    {
+        Task<IEnumerable<GameDto>> GetALLAsync();
+        Task<GameDto?> GetByIdAsync(int id);
+        Task<IEnumerable<GameDto>> GetFeatureAsync();
+        Task<IEnumerable<GameDto>> GetByCategoryAsync(int categoryId);
+        Task<GameDto> CreateAsync(CreateGameDto Dto);
+        Task<GameDto?> updateasync(int id, updateGameDto Dto);
+        Task<bool> DeleteAsync(int id);
+        Task<int> CountAsync();
+    }
+}
